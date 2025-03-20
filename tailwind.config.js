c5ecf99f377b02100d29af88d0b47c0940af5897
Leo <leoo@colostate.edu>
2025-03-19 23:15:42 -0600

    maybe now tailwind work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
-
-  theme: {
-    extend: {}
-  },
-
-  plugins: [],
-  
+const production = !process.env.ROLLUP_WATCH;
+module.exports = {
   future: {
     purgeLayersByDefault: true,
     removeDeprecatedGapUtilities: true,
+  },
+  plugins: [
+
+  ],
+  purge: {
+    content: [
+     "./src/**/*.svelte",
+
+    ],
+    enabled: production // disable purge in dev
   },
 };
