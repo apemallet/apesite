3ec48769074544f7449d00f8bac095da1a73a3d4
Simon Walker <simon@simonwalker.tech>
2025-03-25 09:29:41 -0600

    Revert "tried removing references to /favicon.png"
    This reverts commit 9199aa04fe983f4d2a442bb5a2cac4920cb3a090.

--- src/lib/components/about-us.svelte
+++ src/lib/components/about-us.svelte
@@ -3,26 +3,22 @@ import { base } from '$app/paths';
   const members = {
     Elijah: [
       "ape of high esteem",
-      //"/favicon.png",
-      "",
+      "/favicon.png",
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     ],
     Leo: [
       "ape of high esteem",
-      //"/favicon.png",
-      "",
+      "/favicon.png",
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     ],
     Mason: [
       "ape of high esteem",
-      //"/favicon.png",
-      "",
+      "/favicon.png",
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     ],
     Simon: [
       "ape of high esteem",
-      //"/favicon.png",
-      "",
+      "/favicon.png",
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     ],
   };
