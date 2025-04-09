2a82c69ff2cafcbd377db3d24ca2313003cf5dac
Leo <leoo@colostate.edu>
2025-04-09 14:29:23 -0600

    bye bye div

--- src/lib/components/display.svelte
+++ src/lib/components/display.svelte
@@ -83,52 +83,50 @@
 		<ProductSlide product={products[0]} />
 	{/if}
 
-	<div class="relative pb-15">
-		<div bind:this={sliderRef} class="keen-slider overflow-visible">
-			{#each products as product}
-				<div class="keen-slider__slide opacity-0">
-					<ProductSlide {product} />
-				</div>
-			{/each}
-		</div>
-
-		<!-- Navigation Arrows -->
-		{#if sliderLoaded}
-			<!-- Left Arrow -->
-			<button
-				onclick={prev}
-				onkeydown={(e) => handleKeydown(e, prev)}
-				aria-label="Previous slide"
-				class="absolute top-1/1 left-4/9 -translate-y-1/2 z-2 p-1 border-none bg-transparent focus:outline-none rounded-full invisible sm:visible"
-			>
-				<svg
-					class="w-8 h-8 fill-gray-100 hover:fill-gray-300 hover:scale-120 active:scale-90 transition-all ease-in-out "
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 24 24"
-					aria-hidden="true"
-				>
-					<path
-						d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"
-					/>
-				</svg>
-			</button>
-
-			<!-- Right Arrow -->
-			<button
-				onclick={next}
-				onkeydown={(e) => handleKeydown(e, next)}
-				aria-label="Next slide"
-				class="absolute top-1/1 left-5/9 -translate-y-1/2 z-2 p-1 border-none bg-transparent rounded-full invisible sm:visible"
-			>
-				<svg
-					class="w-8 h-8 fill-gray-100 hover:fill-gray-300 hover:scale-120 active:scale-90 transition-all ease-in-out"
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 24 24"
-					aria-hidden="true"
-				>
-					<path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
-				</svg>
-			</button>
-		{/if}
+	<div bind:this={sliderRef} class="keen-slider overflow-visible pb-15">
+		{#each products as product}
+			<div class="keen-slider__slide opacity-0">
+				<ProductSlide {product} />
+			</div>
+		{/each}
 	</div>
+
+	<!-- Navigation Arrows -->
+	{#if sliderLoaded}
+		<!-- Left Arrow -->
+		<button
+			onclick={prev}
+			onkeydown={(e) => handleKeydown(e, prev)}
+			aria-label="Previous slide"
+			class="absolute top-1/1 left-4/9 -translate-y-1/2 z-2 p-1 rounded-full invisible sm:visible"
+		>
+			<svg
+				class="w-8 h-8 fill-gray-100 hover:fill-gray-300 hover:scale-120 active:scale-90 transition-all ease-in-out"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				aria-hidden="true"
+			>
+				<path
+					d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"
+				/>
+			</svg>
+		</button>
+
+		<!-- Right Arrow -->
+		<button
+			onclick={next}
+			onkeydown={(e) => handleKeydown(e, next)}
+			aria-label="Next slide"
+			class="absolute top-1/1 left-5/9 -translate-y-1/2 z-2 p-1 rounded-full invisible sm:visible"
+		>
+			<svg
+				class="w-8 h-8 fill-gray-100 hover:fill-gray-300 hover:scale-120 active:scale-90 transition-all ease-in-out"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				aria-hidden="true"
+			>
+				<path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+			</svg>
+		</button>
+	{/if}
 </div>
