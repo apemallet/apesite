281b2ceca3b73d649adbd35f4550493b02ae6993
Leo <leoo@colostate.edu>
2025-04-09 12:16:33 -0600

    consistent formatting for titles

--- src/lib/components/about-us.svelte
+++ src/lib/components/about-us.svelte
@@ -1,5 +1,5 @@
 <div class="about-us text-center min-h-50 max-w-7xl py-5">
-  <h1 class="text-4xl pb-10 font-bold text-gray-700">About Us</h1>
+  <h1 class="text-4xl pb-10 font-semibold text-gray-700">About Us</h1>
   <p class="text-lg text-gray-600 font-normal px-5 sm:px-50 pb-4">Apemallet is a creative app development company specializing in sleek, high-performance mobile and web applications. Blending design, innovation, and reliable tech, Apemallet helps clients turn ideas into engaging digital products. </p>
   <p class="text-lg text-gray-600 font-normal px-5 sm:px-50 ">With a collaborative team and a user-first approach, Apemallet delivers scalable, intuitive solutions for startups and enterprises alike—driven by quality, speed, and real-world impact.</p>
 </div>
