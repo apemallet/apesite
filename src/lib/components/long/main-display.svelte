4311ef7ece1111a3b33cf5ee8cecbb1853f4dbf1
Leo <leoo@colostate.edu>
2025-04-12 09:24:14 -0600

    lololol

--- src/lib/components/long/main-display.svelte
+++ src/lib/components/long/main-display.svelte
@@ -1,8 +1,11 @@
 <script>
-  import { base } from "$app/paths";
-  let path = "/long_icon.webp";
+    import { base } from "$app/paths";
+    let img1Path = "/long_icon.webp";
 </script>
 
-<div class="display bg-linear-to-tl from-sky-400 to-purple-600 min-h-lvh">
-  <img src={base + path} alt="long" class="size-40"/>
+<div
+    class="grid grid-cols-2 justify-items-center bg-linear-to-tl from-sky-400 to-purple-600 min-h-lvh py-50"
+>
+    <h1>Long</h1>
+    <img src={base + img1Path} alt="long" class="size-100" />
 </div>
