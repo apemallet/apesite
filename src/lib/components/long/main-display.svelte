c468fb8e01bf4fee2d7f0647052ee160d05f15d1
elredl <leonardo.rodolico03@gmail.com>
2025-05-16 15:43:46 -0600

    Sample page ugly idk how to make it pretty

--- src/lib/components/long/main-display.svelte
+++ src/lib/components/long/main-display.svelte
@@ -1,11 +1,32 @@
 <script>
-    import { base } from "$app/paths";
-    let img1Path = "/long_icon.webp";
+  import { base } from "$app/paths";
+
+  let img1Path = "images/smileMock-portrait.png";
+
+  let long =     {
+      name: "Long",
+      image: "images/long/long_icon.webp",
+      description:
+        "A free universal remote app with slick animations, custom theming, and seamless control across all your devices. Enjoy a smooth and intuitive user experience.",
+      page: "long",
+    };
 </script>
 
 <div
-    class="grid grid-cols-2 justify-items-center bg-linear-to-tl from-sky-400 to-purple-600 min-h-lvh py-50"
+  class="grid grid-cols-2 justify-items-center bg-linear-to-tl from-sky-400 to-purple-600 min-h-lvh py-50"
 >
-    <h1>Long</h1>
-    <img src={base + img1Path} alt="long" class="size-100" />
+  <div class="justify-items-center">
+    <img
+      src={base + long.image}
+      alt="icon"
+      class="object-contain aspect-auto w-40 rounded-4xl"
+    />
+    <h1>{long.name}</h1>
+    <p>{long.description}</p>
+  </div>
+  <img
+    src={base + img1Path}
+    alt="long"
+    class="object-contain aspect-auto w-100"
+  />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,74 +1,75 @@
 <script>
-	import AboutUs from "$lib/components/about-us.svelte";
-	import Display from "$lib/components/display/display-carousel.svelte";
-	import Products from "$lib/components/products.svelte";
+  import AboutUs from "$lib/components/about-us.svelte";
+  import Display from "$lib/components/display/display-carousel.svelte";
+  import Products from "$lib/components/products.svelte";
 
-	let products = [
-        {
-            name: "Long",
-            image: "/long_icon.webp",
-            description:
-                "A free universal remote app with slick animations and custom theming. A free universal remote app with slick animations and custom theming.",
-            page: "long",
-        },
-        {
-            name: "Product 2",
-            image: "/favicon.png",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            page: "long",
-        },
-        {
-            name: "Product 3",
-            image: "/favicon.png",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            page: "long",
-        },
-        {
-            name: "Product 4",
-            image: "/favicon.png",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            page: "long",
-        },
-        {
-            name: "Product 5",
-            image: "/favicon.png",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            page: "long",
-        },
-	]
+  let products = [
+    {
+      name: "Long",
+      image: "images/long/long_icon.webp",
+      description:
+        "A free universal remote app with slick animations, custom theming, and seamless control across all your devices. Enjoy a smooth and intuitive user experience.",
+      page: "long",
+    },
+    {
+      name: "Product 2",
+      image: "images/smileMock.png",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      page: "long",
+    },
+    {
+      name: "Product 3",
+      image: "images/smileMock.png",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      page: "long",
+    },
+    {
+      name: "Product 4",
+      image: "images/smileMock.png",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      page: "long",
+    },
+    {
+      name: "Monkey OG",
+      image: "images/favicon.png",
+      description:
+        "ay bru. jeez og monkey? wtf(reak)?? what. bro eats wifi, speaks in glitch, perchance. 0 chill. 100% banana-coded. tap him wrong and he crash ur fridge.",
+      page: "long",
+    },
+  ];
 </script>
 
-
 <div class="font-poppins text-gray-100 overflow-hidden">
-	<!--  INFO: make sure padding/width of this matches the navbar -->
-	<div class="bg-linear-to-tl from-sky-400 to-purple-600">
-		<div class="max-w-7xl mx-auto px-6 py-4 self-center">
-			<section id="display">
-				<div class="spacer min-h-25 sm:min-h-30 md:min-h-45"></div>
-				<Display {products}/>
-				<div class="spacer min-h-15 sm:min-h-20 md:min-h-30"></div>
-			</section>
-		</div>
-	</div>
-	<div class="mx-auto py-4 bg-gray-100 flex self-center justify-center text-black">
-		<section id="products">
-			<div class="spacer min-h-15 sm:min-h-20 md:min-h-30"></div>
-			<Products {products}/>
-			<div class="spacer min-h-15 sm:min-h-20 md:min-h-30"></div>
-		</section>
-	</div>
-	<div class="mx-auto py-4 self-center bg-gray-100 flex justify-center">
-		<section id="about-us">
-			<div class="spacer min-h-15 sm:min-h-20 md:min-h-30"></div>
-			<AboutUs />
-			<div class="spacer min-h-15 sm:min-h-20 md:min-h-30"></div>
-		</section>
-	</div>
-	<section id="contact">
-		<div class="spacer min-h-lvh"></div>
-	</section>
+  <!--  INFO: make sure padding/width of this matches the navbar -->
+  <div class="bg-linear-to-tl from-sky-400 to-purple-600">
+    <div class="max-w-7xl mx-auto px-6 py-4 self-center">
+      <section id="display">
+        <div class="spacer min-h-25 sm:min-h-30 md:min-h-45"></div>
+        <Display {products} />
+        <div class="spacer min-h-15 sm:min-h-20 md:min-h-30"></div>
+      </section>
+    </div>
+  </div>
+  <div
+    class="mx-auto py-4 bg-gray-100 flex self-center justify-center text-black"
+  >
+    <section id="products">
+      <div class="spacer min-h-15 sm:min-h-20 md:min-h-30"></div>
+      <Products {products} />
+      <div class="spacer min-h-15 sm:min-h-20 md:min-h-30"></div>
+    </section>
+  </div>
+  <div class="mx-auto py-4 self-center bg-gray-100 flex justify-center">
+    <section id="about-us">
+      <div class="spacer min-h-15 sm:min-h-20 md:min-h-30"></div>
+      <AboutUs />
+      <div class="spacer min-h-15 sm:min-h-20 md:min-h-30"></div>
+    </section>
+  </div>
+  <section id="contact">
+    <div class="spacer min-h-lvh"></div>
+  </section>
 </div>
