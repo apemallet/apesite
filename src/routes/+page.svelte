28be95cfd3c29161f9cae925e7b22d9a6b84649f
mason <58895787+masoniis@users.noreply.github.com>
2025-05-17 13:13:36 -0600

    grainy gradient in place

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,7 +18,7 @@
 <svelte:window on:scroll={handleScroll} />
 
 <nav
-  class={`fixed z-10 top-0 left-0 w-full transition-all ease-out 
+  class={`fixed z-10 top-0 left-0 w-full transition-all ease-out
   ${scrollY > 50 ? "bg-[#d6ad3b] shadow-md py-1 text-[#25373a] " : "bg-transparent py-4 text-white"}`}
 >
   <div class="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
@@ -58,15 +58,17 @@
 
 <div class="font-poppins text-gray-100 overflow-hidden">
   <!--  INFO: make sure padding/width of this matches the navbar -->
-  <div class="bg-linear-to-tl from-[#182325] to-[#203535]">
-    <div class="max-w-7xl mx-auto px-6 py-4 self-center">
-      <section id="display">
-        <div class="spacer min-h-25 sm:min-h-30 md:min-h-45"></div>
-        <Display />
-        <div class="spacer min-h-15 sm:min-h-20 md:min-h-30"></div>
-      </section>
-    </div>
-  </div>
+	<div class="bg-gradient-to-tl from-slate-950 via-slate-900 to-slate-800">
+		<div class="absolute inset-0 bg-[url(/svg/grain.svg)] opacity-10 brightness-100 contrast-150">
+		</div>
+		<div class="max-w-7xl mx-auto px-6 py-4 self-center">
+			<section id="display">
+				<div class="spacer min-h-25 sm:min-h-30 md:min-h-45"></div>
+				<Display />
+				<div class="spacer min-h-15 sm:min-h-20 md:min-h-30"></div>
+			</section>
+		</div>
+	</div>
   <div
     class="mx-auto py-4 bg-gray-100 flex self-center justify-center text-black"
   >
