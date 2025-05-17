8bfa906f59b549bec28100aa833423a435d8048b
elredl <leonardo.rodolico03@gmail.com>
2025-05-17 10:06:59 -0600

    Changed theming

--- src/lib/components/display.svelte
+++ src/lib/components/display.svelte
@@ -1,132 +1,132 @@
 <script>
-	import { onMount } from "svelte";
-	import KeenSlider from "keen-slider";
-	import ProductSlide from "./ProductSlide.svelte";
+  import { onMount } from "svelte";
+  import KeenSlider from "keen-slider";
+  import ProductSlide from "./ProductSlide.svelte";
 
-	let sliderRef;
-	let slider;
-	let current = 0;
+  let sliderRef;
+  let slider;
+  let current = 0;
 
-	// Once the slider is loaded, transition it in
-	let sliderLoaded = $state(false);
+  // Once the slider is loaded, transition it in
+  let sliderLoaded = $state(false);
 
-	// Computed property for dot navigation
-	let dotHelper = $derived(
-		slider && slider.track
-			? [...Array(slider.track.details.slides.length).keys()]
-			: [],
-	);
+  // Computed property for dot navigation
+  let dotHelper = $derived(
+    slider && slider.track
+      ? [...Array(slider.track.details.slides.length).keys()]
+      : []
+  );
 
-	let products = [
-		{
-			name: "Long",
-			image: "/long_icon.webp",
-			description:
-				"A free universal remote app with slick animations and custom theming",
-		},
-		{
-			name: "Product 2",
-			image: "/favicon.png",
-			description:
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-		},
-		{
-			name: "Product 3",
-			image: "/favicon.png",
-			description:
-				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-		},
-	];
+  let products = [
+    {
+      name: "Long",
+      image: "/long_icon.webp",
+      description:
+        "A free universal remote app with slick animations and custom theming",
+    },
+    {
+      name: "Product 2",
+      image: "/favicon.png",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      name: "Product 3",
+      image: "/favicon.png",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+  ];
 
-	onMount(() => {
-		slider = new KeenSlider(sliderRef, {
-			loop: true,
-			slides: {
-				perView: 1,
-				spacing: 20,
-			},
-			detailsChanged: (s) => {
-				s.slides.forEach((element, idx) => {
-					element.style.opacity = s.track.details.slides[idx].portion;
-				});
-			},
-			slideChanged: (s) => {
-				current = s.track.details.rel;
-			},
-			defaultAnimation: {
-				duration: 600,
-				easing: (t) => t * (2 - t),
-			},
-		});
+  onMount(() => {
+    slider = new KeenSlider(sliderRef, {
+      loop: true,
+      slides: {
+        perView: 1,
+        spacing: 20,
+      },
+      detailsChanged: (s) => {
+        s.slides.forEach((element, idx) => {
+          element.style.opacity = s.track.details.slides[idx].portion;
+        });
+      },
+      slideChanged: (s) => {
+        current = s.track.details.rel;
+      },
+      defaultAnimation: {
+        duration: 600,
+        easing: (t) => t * (2 - t),
+      },
+    });
 
-		sliderLoaded = true;
+    sliderLoaded = true;
 
-		return () => slider.destroy();
-	});
+    return () => slider.destroy();
+  });
 
-	function prev() {
-		if (slider) slider.prev();
-	}
+  function prev() {
+    if (slider) slider.prev();
+  }
 
-	function next() {
-		if (slider) slider.next();
-	}
+  function next() {
+    if (slider) slider.next();
+  }
 
-	function moveToIdx(idx) {
-		if (slider) slider.moveToIdx(idx);
-	}
+  function moveToIdx(idx) {
+    if (slider) slider.moveToIdx(idx);
+  }
 </script>
 
 <div class="w-full relative">
-	<!-- Until slider is loaded, just show the first product -->
-	{#if !sliderLoaded}
-		<ProductSlide product={products[0]} />
-	{/if}
+  <!-- Until slider is loaded, just show the first product -->
+  {#if !sliderLoaded}
+    <ProductSlide product={products[0]} />
+  {/if}
 
-	<div bind:this={sliderRef} class="keen-slider overflow-visible! pb-15">
-		{#each products as product}
-			<div class="keen-slider__slide opacity-0">
-				<ProductSlide {product} />
-			</div>
-		{/each}
-	</div>
+  <div bind:this={sliderRef} class="keen-slider overflow-visible! pb-15">
+    {#each products as product}
+      <div class="keen-slider__slide opacity-0">
+        <ProductSlide {product} />
+      </div>
+    {/each}
+  </div>
 
-	<!-- Navigation Arrows -->
-	{#if sliderLoaded}
-		<!-- Left Arrow -->
-		<button
-			onclick={prev}
-			onkeydown={(e) => handleKeydown(e, prev)}
-			aria-label="Previous slide"
-			class="absolute top-1/1 left-4/9 -translate-y-1/2 z-2 p-1 rounded-full invisible sm:visible"
-		>
-			<svg
-				class="w-8 h-8 fill-gray-100 hover:fill-gray-300 hover:scale-120 active:scale-90 transition-all ease-in-out"
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 24 24"
-				aria-hidden="true"
-			>
-				<path
-					d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"
-				/>
-			</svg>
-		</button>
+  <!-- Navigation Arrows -->
+  {#if sliderLoaded}
+    <!-- Left Arrow -->
+    <button
+      onclick={prev}
+      onkeydown={(e) => handleKeydown(e, prev)}
+      aria-label="Previous slide"
+      class="absolute top-1/1 left-4/9 -translate-y-1/2 z-2 p-1 rounded-full invisible sm:visible"
+    >
+      <svg
+        class="w-8 h-8 fill-gray-100 hover:fill-gray-300 hover:scale-120 active:scale-90 transition-all ease-in-out cursor-pointer"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        aria-hidden="true"
+      >
+        <path
+          d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"
+        />
+      </svg>
+    </button>
 
-		<!-- Right Arrow -->
-		<button
-			onclick={next}
-			onkeydown={(e) => handleKeydown(e, next)}
-			aria-label="Next slide"
-			class="absolute top-1/1 left-5/9 -translate-y-1/2 z-2 p-1 rounded-full invisible sm:visible"
-		>
-			<svg
-				class="w-8 h-8 fill-gray-100 hover:fill-gray-300 hover:scale-120 active:scale-90 transition-all ease-in-out"
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 24 24"
-				aria-hidden="true"
-			>
-				<path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
-			</svg>
-		</button>
-	{/if}
+    <!-- Right Arrow -->
+    <button
+      onclick={next}
+      onkeydown={(e) => handleKeydown(e, next)}
+      aria-label="Next slide"
+      class="absolute top-1/1 left-5/9 -translate-y-1/2 z-2 p-1 rounded-full invisible sm:visible"
+    >
+      <svg
+        class="w-8 h-8 fill-gray-100 hover:fill-gray-300 hover:scale-120 active:scale-90 transition-all ease-in-out cursor-pointer"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        aria-hidden="true"
+      >
+        <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+      </svg>
+    </button>
+  {/if}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,67 +1,90 @@
 <script>
-	import AboutUs from "$lib/components/about-us.svelte";
-	import Display from "$lib/components/display.svelte";
-	import Products from "$lib/components/products.svelte";
-	let title = "apemallet";
+  import AboutUs from "$lib/components/about-us.svelte";
+  import Display from "$lib/components/display.svelte";
+  import Products from "$lib/components/products.svelte";
 
-	let scrollY = $state(0);
+  import { loadSlim } from "tsparticles-slim";
+  import { onMount } from "svelte";
 
-	function handleScroll() {
-		scrollY = window.scrollY;
-	}
+  let title = "apemallet";
+
+  let scrollY = $state(0);
+
+  function handleScroll() {
+    scrollY = window.scrollY;
+  }
 </script>
 
 <svelte:window on:scroll={handleScroll} />
 
 <nav
-	class={`fixed z-10 top-0 left-0 w-full transition-all ease-out 
-  ${scrollY > 50 ? "bg-black shadow-md py-1" : "bg-transparent py-4"}`}
+  class={`fixed z-10 top-0 left-0 w-full transition-all ease-out 
+  ${scrollY > 50 ? "bg-[#d6ad3b] shadow-md py-1 text-[#25373a] " : "bg-transparent py-4 text-white"}`}
 >
-	<div class="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
-		<div class="text-4xl text-white font-[Baloo]">{title}</div>
-		<ul class="flex font-poppins font-light space-x-6 text-white text-xl">
-			<li>
-				<a href="#display" class="hover:text-gray-200">Home</a>
-			</li>
-			<li>
-				<a href="#products" class="hover:text-gray-200">Products</a>
-			</li>
-			<li>
-				<a href="#about-us" class="hover:text-gray-200">About</a>
-			</li>
-			<li>
-				<a href="#contact" class="hover:text-gray-200">Contact</a>
-			</li>
-		</ul>
-	</div>
+  <div class="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+    <div class="text-4xl font-[Baloo]">{title}</div>
+    <ul class="flex font-poppins font-bold space-x-6 text-l">
+      <li>
+        <a
+          href="#display"
+          class={`${scrollY > 50 ? "hover:text-[#445f63]" : "hover:text-gray-200"}`}
+          >HOME</a
+        >
+      </li>
+      <li>
+        <a
+          href="#products"
+          class={`${scrollY > 50 ? "hover:text-[#445f63]" : "hover:text-gray-200"}`}
+          >PRODUCTS</a
+        >
+      </li>
+      <li>
+        <a
+          href="#about-us"
+          class={`${scrollY > 50 ? "hover:text-[#445f63]" : "hover:text-gray-200"}`}
+          >ABOUT</a
+        >
+      </li>
+      <li>
+        <a
+          href="#contact"
+          class={`${scrollY > 50 ? "hover:text-[#445f63]" : "hover:text-gray-200"}`}
+          >CONTACT</a
+        >
+      </li>
+    </ul>
+  </div>
 </nav>
 
 <div class="font-poppins text-gray-100 overflow-hidden">
-	<!--  INFO: make sure padding/width of this matches the navbar -->
-	<div class="bg-linear-to-tl from-sky-400 to-purple-600">
-		<div class="max-w-7xl mx-auto px-6 py-4 self-center">
-			<section id="display">
-				<div class="spacer min-h-25 sm:min-h-30 md:min-h-45"></div>
-				<Display />
-				<div class="spacer min-h-15 sm:min-h-20 md:min-h-30"></div>
-			</section>
-		</div>
-	</div>
-	<div class="mx-auto py-4 bg-gray-100 flex self-center justify-center text-black">
-		<section id="products">
-			<div class="spacer min-h-15 sm:min-h-20 md:min-h-30"></div>
-			<Products />
-			<div class="spacer min-h-15 sm:min-h-20 md:min-h-30"></div>
-		</section>
-	</div>
-	<div class="mx-auto py-4 self-center bg-gray-100 flex justify-center">
-		<section id="about-us">
-			<div class="spacer min-h-15 sm:min-h-20 md:min-h-30"></div>
-			<AboutUs />
-			<div class="spacer min-h-15 sm:min-h-20 md:min-h-30"></div>
-		</section>
-	</div>
-	<section id="contact">
-		<div class="spacer min-h-lvh"></div>
-	</section>
+  <!--  INFO: make sure padding/width of this matches the navbar -->
+  <div class="bg-linear-to-tl from-[#182325] to-[#203535]">
+    <div class="max-w-7xl mx-auto px-6 py-4 self-center">
+      <section id="display">
+        <div class="spacer min-h-25 sm:min-h-30 md:min-h-45"></div>
+        <Display />
+        <div class="spacer min-h-15 sm:min-h-20 md:min-h-30"></div>
+      </section>
+    </div>
+  </div>
+  <div
+    class="mx-auto py-4 bg-gray-100 flex self-center justify-center text-black"
+  >
+    <section id="products">
+      <div class="spacer min-h-15 sm:min-h-20 md:min-h-30"></div>
+
+      <Products />
+      <div class="spacer min-h-15 sm:min-h-20 md:min-h-30"></div>
+    </section>
+  </div>
+  <div class="mx-auto py-4 self-center bg-gray-100 flex justify-center">
+    <section id="about-us">
+      <div class="spacer min-h-15 sm:min-h-20 md:min-h-30"></div>
+      <AboutUs />
+      <div class="spacer min-h-15 sm:min-h-20 md:min-h-30"></div>
+    </section>
+  </div>
+  <section id="contact">
+    <div class="spacer min-h-lvh"></div>
+  </section>
 </div>
