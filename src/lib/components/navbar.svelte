d3e8e250ab0b5dbce9c4e31a5353b21d33615397
Leo <leoo@colostate.edu>
2025-05-17 20:12:37 -0600

    main page

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -5,7 +5,7 @@
 
 <nav
   class={`fixed z-10 top-0 left-0 w-full transition-all ease-out
-  ${scrollY > 50 ? "bg-[#d6ad3b] shadow-md py-1 text-[#25373a] " : "bg-transparent py-4 text-white"}`}
+  ${scrollY > 50 ? "bg-[#d6ad3b] shadow-md py-1 text-[#25373a] " : "bg-transparent py-4 text-[#d6ad3b]"}`}
 >
   <div class="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
     <div class="text-4xl font-[Baloo]">{title}</div>
@@ -13,28 +13,28 @@
       <li>
         <a
           href="#display"
-          class={`${scrollY > 50 ? "hover:text-[#445f63]" : "hover:text-gray-200"}`}
+          class={`${scrollY > 50 ? "hover:text-[#445f63]" : "hover:text-yellow-300"}`}
           >HOME</a
         >
       </li>
       <li>
         <a
           href="#products"
-          class={`${scrollY > 50 ? "hover:text-[#445f63]" : "hover:text-gray-200"}`}
+          class={`${scrollY > 50 ? "hover:text-[#445f63]" : "hover:text-yellow-300"}`}
           >PRODUCTS</a
         >
       </li>
       <li>
         <a
           href="#about-us"
-          class={`${scrollY > 50 ? "hover:text-[#445f63]" : "hover:text-gray-200"}`}
+          class={`${scrollY > 50 ? "hover:text-[#445f63]" : "hover:text-yellow-300"}`}
           >ABOUT</a
         >
       </li>
       <li>
         <a
           href="#contact"
-          class={`${scrollY > 50 ? "hover:text-[#445f63]" : "hover:text-gray-200"}`}
+          class={`${scrollY > 50 ? "hover:text-[#445f63]" : "hover:text-yellow-300"}`}
           >CONTACT</a
         >
       </li>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
   import AboutUs from "$lib/components/sections/about_us.svelte";
   import Display from "$lib/components/sections/product_display.svelte";
-  import Products from "$lib/components/products.svelte";
+  import Products from "$lib/components/sections/products.svelte";
 	import Navbar from "$lib/components/navbar.svelte"
 
   import { onMount } from "svelte";
@@ -19,14 +19,13 @@
 
 <div class="font-poppins text-gray-100 overflow-hidden">
   <!--  INFO: make sure padding/width of this matches the navbar -->
-    <div class="bg-gradient-to-tl from-slate-950 via-slate-900 to-slate-800">
-      <div class="absolute inset-0 bg-[url(/svg/grain.svg)] opacity-10 brightness-100 contrast-150">
-      </div>
-      <div class="max-w-7xl mx-auto h-200 px-6 py-4 justify-center flex flex-col items-center">
+  <div class="bg-gradient-to-tl from-slate-950 via-slate-900 to-slate-800">
+    <div class="absolute inset-0 bg-[url(/svg/grain.svg)] opacity-10 brightness-100 contrast-150"></div>
+      <div class="max-w-7xl mx-auto h-174 px-6 py-4 justify-center flex flex-col items-center">
         <h1 class="text-7xl text-center font-[Baloo] text-[#d6ad3b] pb-15">apemallet</h1>
-        <h2 class=" max-w-xl text-xl text-center font-poppins text-slate-900 bg-[#d6ad3b] rounded-xl p-10 font-semibold">Apemallet is a creative app development company specializing in sleek, high-performance mobile and web applications. Blending design, innovation, and reliable tech, Apemallet helps clients turn ideas into engaging digital products. </h2>
-      </div>
+        <h2 class=" max-w-xl text-xl text-center font-poppins text-slate-900 bg-[#d6ad3b] rounded-xl p-7 font-semibold"> Nothing but monkey business </h2>
     </div>
+  </div>
 
 	<div class="bg-gradient-to-tl from-slate-950 via-slate-900 to-slate-800">
 		<div class="absolute inset-0 bg-[url(/svg/grain.svg)] opacity-10 brightness-100 contrast-150">
