2b746e5b0325c6b30524b79a2d9a6e776621412a
Leo <leoo@colostate.edu>
2025-05-17 22:15:07 -0600

    mobile navbar

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,43 +1,84 @@
 <script lang="ts">
 	let { scrollY } : { scrollY: number } = $props();
 	const title = "apemallet";
+
+  let menuOpen = $state(false);
+
+  function clickOutside(node) {
+    const handleClick = (event) => {
+      if (!node.contains(event.target)) {
+        menuOpen = false;
+      }
+    };
+
+    document.addEventListener("click", handleClick, true);
+
+    return {
+      destroy() {
+        document.removeEventListener("click", handleClick, true);
+      },
+    };
+  }
+
+  let pages = [
+    { name: "HOME", href: "#display" },
+    { name: "PRODUCTS", href: "#products" },
+    { name: "ABOUT", href: "#about-us" },
+    { name: "CONTACT US", href: "#contact" },
+  ];
 </script>
 
 <nav
-  class={`fixed z-10 top-0 left-0 w-full transition-all ease-out
-  ${scrollY > 50 ? "bg-[#d6ad3b] shadow-md py-1 text-[#25373a] " : "bg-transparent py-4 text-[#d6ad3b]"}`}
+  class={`fixed z-10 top-0 left-0 w-full
+    ${scrollY > 50 
+      ? "bg-[#d6ad3b] text-[#25373a] shadow-md sm:transition-all sm:ease-out sm:py-1" 
+      : "bg-[#d6ad3b] text-[#25373a] sm:bg-transparent sm:py-4 sm:text-[#d6ad3b] sm:transition-all sm:ease-out"
+    }`}
 >
   <div class="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
     <div class="text-4xl font-[Baloo]">{title}</div>
     <ul class="flex font-poppins font-bold space-x-6 text-l">
-      <li>
+      <li class="block sm:hidden relative" use:clickOutside>
+        <button
+          onclick={() => menuOpen = !menuOpen}
+          aria-label="Menu"
+          class={`${scrollY > 50 ? "text-[#25373a] hover:text-[#445f63]" : "sm:py-4 text-[#25373a] sm:text-[#d6ad3b]"}`}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              d="M0,3.875c0-1.104,0.896-2,2-2h20.75c1.104,0,2,0.896,2,2s-0.896,2-2,2H2C0.896,5.875,0,4.979,0,3.875z 
+              M22.75,10.375H2c-1.104,0-2,0.896-2,2c0,1.104,0.896,2,2,2h20.75c1.104,0,2-0.896,2-2C24.75,11.271,23.855,10.375,22.75,10.375z 
+              M22.75,18.875H2c-1.104,0-2,0.896-2,2s0.896,2,2,2h20.75c1.104,0,2-0.896,2-2S23.855,18.875,22.75,18.875z"
+            />
+          </svg>
+        </button>
+      
+        {#if menuOpen}
+          <ul class="fixed top-15 left-0 w-screen bg-[#d6ad3b] text-[#25373a] z-50 overflow-auto p-6 space-y-4">
+            {#each pages as page}
+              <li class="border-b last:border-none">
+                <a href={page.href} class="block text-xl px-4 py-2 hover:bg-gray-100 rounded">
+                  {page.name}
+                </a>
+              </li>
+            {/each}
+          </ul>
+        {/if}
+      </li>
+      {#each pages as page}
+      <li class="hidden sm:block">
         <a
-          href="#display"
+          href={page.href}
           class={`${scrollY > 50 ? "hover:text-[#445f63]" : "hover:text-yellow-300"}`}
-          >HOME</a
+          >{page.name}</a
         >
       </li>
-      <li>
-        <a
-          href="#products"
-          class={`${scrollY > 50 ? "hover:text-[#445f63]" : "hover:text-yellow-300"}`}
-          >PRODUCTS</a
-        >
-      </li>
-      <li>
-        <a
-          href="#about-us"
-          class={`${scrollY > 50 ? "hover:text-[#445f63]" : "hover:text-yellow-300"}`}
-          >ABOUT</a
-        >
-      </li>
-      <li>
-        <a
-          href="#contact"
-          class={`${scrollY > 50 ? "hover:text-[#445f63]" : "hover:text-yellow-300"}`}
-          >CONTACT</a
-        >
-      </li>
+      {/each}
     </ul>
   </div>
 </nav>
