45591cdb23f7acbfe5a58ea20f20525fe725a962
mason <58895787+masoniis@users.noreply.github.com>
2025-05-18 00:55:25 -0600

    segregated contact

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script>
   import Display from "$lib/components/sections/product_carousel/index.svelte";
   import AboutUs from "$lib/components/sections/about_us.svelte";
+	import Contact from "$lib/components/sections/contact.svelte";
   import Main from "$lib/components/sections/main.svelte";
 	import Divider from "$lib/components/divider.svelte";
 </script>
 
-<div class="font-poppins text-gray-100 overflow-hidden">
+<div class="font-poppins overflow-hidden">
 	<!-- INFO: main "APEMALLET" section -->
 	<div class="relative bg-gradient-to-tl from-apeium-surface via-apeium-surfacetint to-apeium-surfacetintier">
 		<div class="absolute inset-0 bg-[url(/svg/grain.svg)] opacity-10 brightness-100 contrast-150"></div>
@@ -15,7 +16,7 @@
 	</div>
 
 	<!-- INFO: product carousel -->
-	<div class="relative bg-apeium-950">
+	<div class="relative bg-apeium-surface text-gray-100">
 		<div class="absolute inset-0 bg-[url(/svg/grain.svg)] opacity-20 brightness-50 contrast-150"></div>
 		<Divider />
 		<div class="max-w-7xl mx-auto py-24 self-center">
@@ -27,7 +28,7 @@
 	</div>
 
 	<!-- INFO: about -->
-	<div class="bg-apeium-700">
+	<div class="bg-apeium-surface text-gray-100">
 		<div class="mx-auto py-4 self-center flex justify-center">
 			<section id="about-us">
 				<AboutUs />
@@ -36,7 +37,11 @@
 	</div>
 
 	<!-- INFO: contact -->
-  <section id="contact">
-    <div class="spacer min-h-lvh"></div>
-  </section>
+	<div class="bg-apecent-surface text-apeium-surface">
+		<div class="mx-auto py-4 self-center flex justify-center">
+			<section id="contact">
+				<Contact />
+			</section>
+		</div>
+	</div>
 </div>
