bbb26f026a57a3383fd9ed7ae64a0fc95738d2a4
mason <58895787+masoniis@users.noreply.github.com>
2025-05-19 10:37:58 -0600

    make it so year in footer isn't hardcoded

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,7 +1,7 @@
 <footer class="bg-apecent-surface text-apeium-surface">
 	<div class="mx-auto max-w-7xl px-6 py-6 flex items-center justify-between w-full">
 		<div class="text-xs leading-5 text-primaryfg">
-			&copy; Apemallet Limited 2025
+			&copy; Apemallet Limited {new Date().getFullYear()}
 		</div>
 		<div class="flex justify-center text-xs space-x-6 2xs:order-2">
 			<p> thing here </p>
