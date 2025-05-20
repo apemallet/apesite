5a5fd78e6e1f5125de7d2bdae3401af000218905
mason <58895787+masoniis@users.noreply.github.com>
2025-05-20 12:26:25 -0600

    coupled height to viapos as well

--- src/lib/components/sections/product_carousel/index.svelte
+++ src/lib/components/sections/product_carousel/index.svelte
@@ -124,12 +124,13 @@
 <Divider viaPosition={$viaPos}/>
 
 <div class="relative">
-  <button id="ribbonButton" class="absolute right-4 md:right-32 md:flex cursor-pointer h-10 hover:h-14"
+  <button id="ribbonButton" class="absolute right-4 md:right-32 md:flex cursor-pointer"
 		onmouseenter={() => viaPos.set(90)}
 		onmouseleave={() => viaPos.set(50)}
     onfocus={() => viaPos.set(90)}
     onblur={() => viaPos.set(50)}
 		style:opacity="{$viaPos / 100}"
+		style:height="{($viaPos)/25}rem"
     onclick={toggleAllProducts}
   >
     <div class="grid place-items-center">
@@ -144,10 +145,3 @@
     <HangRibbon/>
   </button>
 </div>
-
-<style lang="postcss">
-  #ribbonButton {
-    transition-property: height;
-    transition-duration: 300ms;
-  }
-</style>
