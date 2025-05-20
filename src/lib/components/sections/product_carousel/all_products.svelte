a57912b207e81718892852e2c903187f6c315f82
mason <58895787+masoniis@users.noreply.github.com>
2025-05-19 21:52:45 -0600

    mo bile optimization attempts

--- src/lib/components/sections/product_carousel/all_products.svelte
+++ src/lib/components/sections/product_carousel/all_products.svelte
@@ -6,11 +6,13 @@
 <div
   class="grid grid-cols-2 sm:grid-cols-4 gap-5 gap-y-15 pt-15 max-w-7xl px-5 sm:px-0"
 >
-  {#each products as product}
+  {#each products as product (product.name)}
     <div class="grid grid-cols-1 gap-y-2 justify-items-center">
       <image
         src={base + product.image}
         alt={product.name}
+				width="200"
+				height="200"
         class="rounded-4xl size-40 cursor-pointer transition-all ease-in-out hover:scale-105 active:scale-90"
       />
       <h1 class="font-semibold text-2xl text-center">
